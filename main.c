f0ef9f0244284cfb08c950b5abfaee6b86a79a51
Jansen-Alves <75702771+Jansen-Alves@users.noreply.github.com>
2023-10-27 09:26:57 -0300

    Update main.c

--- main.c
+++ main.c
@@ -21,34 +21,27 @@ void menu(FILE *meta, FILE *clientes, int modo){
             printf("Digite o nome do cliente que você deseja inserir:\n");
             scanf("%s", nome);
             novo = criarCliente(chave, nome);
-            inserir(meta, clientes, novo);
+            inserirCliente(clientes, novo, modo);
             free(novo);
             //printf("fechando arquivos e ponteiros");
             break;
         case (2):
             printf("Digite a chave do cliente que você quer deletar:\n");
             scanf("%d", &chave);
-            deletar(meta, clientes, chave);
+            deletar(clientes, chave, modo);
             free(novo);
             break;
         case (3):
             printf("Digite a chave do cliente que você quer buscar:\n");
             scanf("%d", &chave);
     
-            novo = busca(clientes, chave);
+            novo = buscarCliente(clientes, chave, modo);
             if(novo->chave == -1){
                 printf("Cliente não encontrado,por favor insira ele na Hash primeiro");
             }
             else{
                 printf("> Chave do cliente: %d \n", novo->chave);
                 printf("> Nome do cliente: %s \n", novo->nome);
-                if(novo->prox != -1){
-                    printf("%d", novo->prox);
-                    printf("> Proximo cliente dessa faixa se encontra no ' %d ' lugar", novo->prox);
-                }
-                else{
-                    printf("Esse cliente se encontra no final da fila");
-                }
             }
             break;
         case (4):
